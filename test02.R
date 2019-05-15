be1639eef5cf335fd662e780278c737b5204447b
GDelaCruzA <gaguero@ipn.mx>
2019-05-15 17:56:00 -0600

    primer commit en el script de R para configurar

---
+++ test02.R
@@ -0,0 +1,10 @@
+# script para agregarlo al proyecto y procesar los comandos de configuración
+# de git y github de acuerdo al Cap 10 de happygitwithR
+# 
+# aqui vamos moviendo todo desde RStudio
+# guardamos, Git > Staged
+#            Git > Commit
+# 
+# el push hacerlo a mano y lo siguiente
+# 
+# 
